f8237b3be0cfd3dae3bfa62190346a37151ba931
Melodyra <rafiattariq346@gmail.com>
2026-01-29 22:17:05 +0700

    Update CV + Tutorial

--- Assets/Script/startgame.cs
+++ Assets/Script/startgame.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class startgame : MonoBehaviour
 {
+    public AudioSource soundEffect;
+    private int sceneId; 
+
     public void MovetoScene(int id)
     {
-        SceneManager.LoadScene(id);
+        sceneId = id; 
+        if (soundEffect != null) soundEffect.Play();
+        Invoke("Load", 1f); 
+    }
+
+    void Load()
+    {
+        SceneManager.LoadScene(sceneId);
     }
 }
